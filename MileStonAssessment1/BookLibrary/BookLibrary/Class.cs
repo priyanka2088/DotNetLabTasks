5a61ed3d54b66ecfaa5b16c2de4a5671b3fddb64
priyanka2088 <priyankak2088@gmail.com>
2024-08-03 18:41:00 +0530

    Assessment submitted

---
+++ MileStonAssessment1/BookLibrary/BookLibrary/Class.cs
@@ -0,0 +1,147 @@
+﻿namespace BookLibrary
+{
+    public class Book
+    {
+            //properties
+            public int BookID{ get; set; }
+            public string BookName{ get; set; }
+            public string ISBNNo{ get; set; }
+            public double Price{ get; set; }
+            public string Publisher{ get; set; }
+            public int NumberOfPages{ get; set; }
+            public string Language{ get; set; }
+            public string LoT{ get; set; }
+            public string Summary{ get; set; }
+
+            //constructor
+            public Book(int id, string name, string isbn, double price, string publisher,
+                        int pages, string language = "English", string lot = "Technical", string summary = "")
+            {
+                BookID = id;
+                BookName = name;
+                ISBNNo = isbn;
+                Price = price;
+                Publisher = publisher;
+                NumberOfPages = pages;
+                Language = language;
+                LoT = lot;
+                Summary = summary;
+            }
+
+            //AddBookDetails Method
+            public void AddBookDetails()
+            {
+
+                bool IsValidIdData = false;
+                while (!IsValidIdData)
+                {
+                    Console.WriteLine("Enter The 5 Digit Book ID : ");
+                    string bookId = Console.ReadLine();
+                    //Checking BookId is Null or Empty
+                    if (string.IsNullOrEmpty(bookId))
+                    {
+                       Console.WriteLine("Book ID cannot be Null or Empty,Please Enter Correct Book Id");
+                    }
+                    else
+                    {
+                        bool IsInteger = int.TryParse(bookId, out int parsedId);
+
+                        //Checking BookID is 5 Digit or Not
+                        if (IsInteger && bookId.Length == 5)
+                        {
+                            BookID = parsedId;
+                            IsValidIdData = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Book Id. Please enter 5-digit Book Id number.");
+                        }
+                    }
+                }
+
+            bool IsValidNameData = false;
+            while (!IsValidNameData)
+            {
+                Console.WriteLine("Enter Book Name: ");
+                BookName = Console.ReadLine();
+                //Checking Book Name is Null or Empty
+                if (string.IsNullOrEmpty(BookName))
+                {
+                    Console.WriteLine("Book Name cannot be Null or Empty, Please Enter Correct Book Name");
+                }
+                else
+                {
+                    IsValidNameData = true;
+                }
+            }
+
+                Console.WriteLine("Enter ISBN Number: ");
+                ISBNNo = Console.ReadLine();
+
+            //Validating Price Type
+            bool IsVaildPrice = false;
+            while (!IsVaildPrice)
+            {
+                Console.WriteLine("Enter Price: ");
+                String priceValue = Console.ReadLine();
+                IsVaildPrice = double.TryParse(priceValue, out double parcedPrice);
+                if (IsVaildPrice)
+                {
+                    Price = parcedPrice;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Price in correct Format");
+                }
+
+            }
+
+            Console.WriteLine("Enter Publisher: ");
+                Publisher = Console.ReadLine();
+
+            //Validating PageNo Type
+            bool IsVaildPageNo = false;
+            while (!IsVaildPageNo)
+            {
+                Console.WriteLine("Enter Number of Pages: ");
+                String pageNo = Console.ReadLine();
+                IsVaildPageNo = int.TryParse(pageNo, out int parcedPageNo);
+                if (IsVaildPageNo)
+                {
+                    NumberOfPages = parcedPageNo;
+                }
+                else
+                {
+                    Console.WriteLine("Enter No of Pages in Correct Format");
+                }
+
+            }
+
+                // Validation for Language and LoT
+                Console.WriteLine("Enter Language (Press Enter  for default 'English'): ");
+                Language = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(Language))
+                {
+                    Language = "English";
+                }
+
+                bool IsValidLotData = false;
+                while (!IsValidLotData)
+                {
+                    Console.WriteLine("Enter LoT (.NET, Java, IMS, V&V, BI, RDBMS): ");
+                    LoT = Console.ReadLine();
+
+                    
+                    // Check if LoT is Present in the Specified Value
+                    if (LoT.ToLower() == ".net" || LoT.ToLower() == "java" || LoT.ToLower() == "ims" || LoT.ToLower() == "v&v" || LoT.ToLower() == "bi" || LoT.ToLower() == "rdbms")
+                    {
+                        IsValidLotData = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid LoT. Please choose one from the given List: .NET, Java, IMS, V&V, BI, RDBMS.");
+                    }
+                }
+            }
+        }
+    }
---
+++ MileStonAssessment1/BookLibrary/BookManagementSystem/Program.cs
@@ -0,0 +1,111 @@
+﻿using BookLibrary;
+namespace BookManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating List of Book Class Objects 
+            List<Book> books = new List<Book>();
+            Console.WriteLine("=========Book Management system=========");
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Add a Book");
+                Console.WriteLine("2. Display all Books");
+                Console.WriteLine("3. Delete a Book by ID");
+                Console.WriteLine("4. Exit");
+                Console.Write("Choose your Option: ");
+
+                char option;
+                if (!char.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Please Enter correct Option");
+                }
+                else
+                {
+
+                    switch (option)
+                    {
+                        case '1':
+                            AddBook(books);
+                            break;
+                        case '2':
+                            DisplayBooks(books);
+                            break;
+                        case '3':
+                            DeleteBook(books);
+                            break;
+                        case '4':
+                            Console.WriteLine("Thank you, Visit again");
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice. Please enter a number between 1 and 4.");
+                            break;
+                    }
+                }
+            }
+        }
+
+        // Add New Book
+        static void AddBook(List<Book> books)
+        {
+            Book newBook = new Book(0, "", "", 0.0, "", 0); 
+            newBook.AddBookDetails();                    
+            books.Add(newBook);                             
+            Console.WriteLine("Book added successfully.");
+        }
+
+        // Display all books
+        static void DisplayBooks(List<Book> books)
+        {
+            if (books.Count == 0)
+            {
+                Console.WriteLine("No books available.");
+                return;
+            }
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"Book ID: {book.BookID}, Book Name: {book.BookName}, ISBN No: {book.ISBNNo}, Price: {book.Price}, " +
+                                  $"Publisher: {book.Publisher},No of Pages: {book.NumberOfPages}, Language: {book.Language}, " +
+                                  $"LoT: {book.LoT}, Summary: {book.Summary}");
+            }
+        }
+
+        //Delete Book by ID
+        static void DeleteBook(List<Book> books)
+        {
+            Console.WriteLine("Enter the Book ID to delete: ");
+            string id = Console.ReadLine();
+            int bookId;
+            bool ISValidId = int.TryParse(id,out bookId);
+            while (!ISValidId)
+            {
+                Console.WriteLine("Enter Book ID in Correct Format :");
+                id= Console.ReadLine();
+                if (id.Length == 5)
+                {
+                    ISValidId = int.TryParse(id, out bookId);
+                }
+                else
+                {
+                    ISValidId = false;
+                }
+                
+            }
+    
+            Book filteredBook = books.Find(b => b.BookID == bookId);        
+            if ( filteredBook!= null)
+            {
+                books.Remove(filteredBook);
+                Console.WriteLine("Book deleted successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Book not found.");
+            }
+        }
+    }
+}
