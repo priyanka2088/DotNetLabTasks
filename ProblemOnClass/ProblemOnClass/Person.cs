9c52ec8c3add53886c152c8fd01a4609a4675db3
priyanka2088 <priyankak2088@gmail.com>
2024-08-08 15:07:31 +0530

    array of class commit

---
+++ ProblemOnClass/ProblemOnClass/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemOnClass
+{
+    class Person
+    {
+        public string Name;
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+        ~Person() 
+        {
+            this.Name=string.Empty;
+        }
+        public override string ToString()
+        {
+            return "Hello My Name is " + Name;
+        }
+    }
+}
---
+++ ProblemOnClass/ProblemOnClass/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ProblemOnClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[3];
+            string name;
+            for(int i=0;i<3;i++)
+            {
+               Console.WriteLine("Enter the Name");
+                name = Console.ReadLine();
+                person[i] = new Person(name);
+            }
+            for(int i=0;i<3; i++)
+            {
+                Console.WriteLine(person[i].ToString());
+            }
+
+        }
+    }
+}
